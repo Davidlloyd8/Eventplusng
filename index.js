2a9ad8920c5c344bb6b3628ba7a0cc469ba5fb9a
Tiwatope <oniogokristi@gmail.com>
2022-01-16 20:07:18 +0100

    database update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-app.js";
-import { getDatabase, set, ref, update } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-database.js";
+import { getDatabase, set, ref, update, onValue } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-database.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -9,6 +9,7 @@ const firebaseConfig = {
   //yout config code
   apiKey: "AIzaSyCuscSxshALFmKUYSyRlebDLjfz-zX9XWc",
   authDomain: "eventplus-vs1.firebaseapp.com",
+  databaseURL: "https://eventplus-vs1-default-rtdb.firebaseio.com",
   projectId: "eventplus-vs1",
   storageBucket: "eventplus-vs1.appspot.com",
   messagingSenderId: "430767169041",
@@ -20,36 +21,44 @@ const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 const auth = getAuth();
 
+
+
 const user = auth.currentUser;
 onAuthStateChanged(auth, (user) => {
   if (user) {
     // User is signed in, see docs for a list of available properties
     // https://firebase.google.com/docs/reference/js/firebase.User
-    const uid = user.uid;
+    const id=user.uid;
     document.getElementById("anotherhide").style.display="none";
     document.getElementById("logout").style.display="inline";
-    window.alert("welcome "+user.email)
+    const username= ref(database, 'users/' + id + '/username');
+    onValue(username, (snapshot) => {
+        var data = snapshot.val();
+        window.alert('Welcome Back ... '+data);
+    });
+    //bla bla bla
+    // ...
     logout.addEventListener('click',(e)=>{
+
         signOut(auth).then(() => {
             // Sign-out successful.
-            alert('user logged out');
-            
+            onValue(username, (snapshot) => {
+                var data = snapshot.val();
+                alert('Goodbye ... '+data);
+            });
             document.getElementById("anotherhide").style.display="inline";
             document.getElementById("logout").style.display="none";
-            }).catch((error) => {
+            // window.location="index.html"
+        }).catch((error) => {
             // An error happened.
             const errorCode = error.code;
             const errorMessage = error.message;
             window.alert(errorMessage);
         });
-    
     });
-        
-    // window.location="index copy.html"
-    //bla bla bla
-    // ...
   } else {
     // User is signed out
+    // ...
+    //bla bla bla
   }
 });
-
